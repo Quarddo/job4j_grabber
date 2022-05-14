6f085661f3eeceb780d7e43b3bca56404e214ef1
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-05-14 20:30:26 +0500

    2.2. Модель данных - Post. [#285211 #300870]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -18,7 +18,7 @@ public class Post {
         this.created = created;
     }
 
-    public Post(String title, String link, String description,LocalDateTime created) {
+    public Post(String title, String link, String description, LocalDateTime created) {
         this.title = title;
         this.link = link;
         this.description = description;
